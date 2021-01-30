1fe58f84fd8c4bb5b64b99d24b087a99cf540763
Anastasia-Nayita <kekelidou@gmail.com>
2021-01-30 22:12:17 +0100

    rendering dynamic num of tasks left, added toggle of dark/ligth theme

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
 let newTaskInp = $(".new-task__txt");
 const tasksList = $(".tasks-list");
+const iLeft = $(".i-left");
+const themeIcon = $("#theme-icon");
+const body = $("body");
+const bg = $(".bg");
 let myHtml = "";
 let i = 0;
 newTaskInp.on("keypress", function (e) {
-  if (e.which === 13) {
+  if (e.which === 13 && newTaskInp.val() !== "") {
     i++;
     let addedTask =
       "<div class='tasks-list__i'><div class='round'> <input id='checkbox " +
       `${i}` +
-      "' type='checkbox' /><label for='checkbox " +
+      "' type='checkbox' name='checkbox' /><label id='label' for='checkbox " +
       `${i}` +
-      "'>" +
-      " <div class='task-list__txt'>" +
+      "'><div class='task-list__txt'>" +
       newTaskInp.val() +
-      "</div>" +
-      "</label></div>" +
-      "</div>";
+      "</div></label></div></div>";
     myHtml += addedTask;
     tasksList.html(myHtml);
     newTaskInp.val("");
+    iLeft.html(i + " items left");
+  }
+  $("input").on("click", function (e) {
+    if ($(this).is(":checked") && i !== 0) {
+      i--;
+      iLeft.html(i + " items left");
+    } else {
+      i++;
+      iLeft.html(i + " items left");
+    }
+  });
+});
+
+themeIcon.on("click", function () {
+  body.toggleClass("dark-theme");
+  if (body.hasClass("dark-theme")) {
+    themeIcon.attr("src", "./images/icon-sun.svg");
+    bg.attr("src", "./images/bg-desktop-dark.jpg");
+  } else {
+    themeIcon.attr("src", "./images/icon-moon.svg");
+    bg.attr("src", "./images/bg-desktop-light.jpg");
   }
 });

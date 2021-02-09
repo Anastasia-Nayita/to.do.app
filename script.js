330b1af1c422df22b21310cfb25028717f37328d
Anastasia-Nayita <kekelidou@gmail.com>
2021-02-09 14:45:54 +0100

    fixing localstorage

--- script.js
+++ script.js
@@ -5,19 +5,26 @@ const themeIcon = $("#theme-icon");
 const body = $("body");
 const bg = $(".bg");
 let myHtml = "";
-let i = 0;
-
+let oldTasks = "";
+let i = localStorage.length;
 $(window).on("load", function () {
-  for (j = 0; j > 0; j++) {
-    j++;
-    //console.log(j);
-    tasksList.html(localStorage.getItem("myHtmlStored" + `${j}`));
+  let lastIndex = localStorage.getItem("lastIndex");
+
+  // check if it exists
+  if (lastIndex) {
+    // load it if it does exist
+    myHtml += localStorage.getItem(`myHtmlStored${lastIndex}`); // noticed I used lastIndex
+    tasksList.html(myHtml);
+    i = Number(lastIndex) + 1; // you need to update i so as to update your item left. You need to add 1, remember your index starts from 0. NOTE: you might need to change this when you update your click event to calculate items left
+    iLeft.html(i + " items left");
   }
+
+  
 });
 
 newTaskInp.on("keypress", function (e) {
   if (e.which === 13 && newTaskInp.val() !== "") {
-    i++;
+    console.log("i = ", i);
     let addedTask =
       "<div class='tasks-list__i'><div class='round'> <input id='checkbox " +
       `${i}` +
@@ -30,19 +37,23 @@ newTaskInp.on("keypress", function (e) {
     tasksList.html(myHtml);
     try {
       localStorage.setItem("myHtmlStored" + `${i}`, myHtml);
-      // console.log(
-      //   "local storage: ",
-      //   localStorage.getItem("myHtmlStored" + `${i}`)
-      // );
-    } catch (e) {
-      console.log("What a nuisance");
+      localStorage.setItem("lastIndex", i);
+      i++;
+    } catch (err) {
+      console.log(err);
     }
+
     newTaskInp.val("");
+    console.log(`first items left ${i}`);
+
     iLeft.html(i + " items left");
   }
+
   $("input").on("click", function (e) {
+    console.log("clicked");
     if ($(this).is(":checked") && i !== 0) {
       i--;
+      console.log("i in the checked", i);
       iLeft.html(i + " items left");
     } else {
       i++;

ea1846ce2ed4018bf633f940960f5eb17e496c32
Olaleye Blessing <olayinkablexxy@gmail.com>
2021-02-09 12:28:57 +0100

    Fixed localStorage lists

--- script.js
+++ script.js
@@ -8,56 +8,74 @@ let myHtml = "";
 let i = 0;
 
 $(window).on("load", function () {
-  for (j = 0; j > 0; j++) {
-    j++;
-    //console.log(j);
-    tasksList.html(localStorage.getItem("myHtmlStored" + `${j}`));
-  }
+    // please, don't read this first, come back to it!!
+
+    // get your last index
+    let lastIndex = localStorage.getItem("lastIndex");
+
+    // check if it exists
+    if (lastIndex) {
+        // load it if it does exist
+        console.log("yes");
+        myHtml += localStorage.getItem(`myHtmlStored${lastIndex}`); // noticed I used lastIndex
+        tasksList.html(myHtml);
+        i = Number(lastIndex) + 1; // you need to update i so as to update your item left. You need to add 1, remember your index starts from 0. NOTE: you might need to change this when you update your click event to calculate items left
+        iLeft.html(i + " items left"); 
+    }
 });
 
 newTaskInp.on("keypress", function (e) {
-  if (e.which === 13 && newTaskInp.val() !== "") {
-    i++;
-    let addedTask =
-      "<div class='tasks-list__i'><div class='round'> <input id='checkbox " +
-      `${i}` +
-      "' type='checkbox' name='checkbox' /><label id='label' for='checkbox " +
-      `${i}` +
-      "'><div class='task-list__txt'>" +
-      newTaskInp.val() +
-      "</div></label></div></div>";
-    myHtml += addedTask;
-    tasksList.html(myHtml);
-    try {
-      localStorage.setItem("myHtmlStored" + `${i}`, myHtml);
-      // console.log(
-      //   "local storage: ",
-      //   localStorage.getItem("myHtmlStored" + `${i}`)
-      // );
-    } catch (e) {
-      console.log("What a nuisance");
+    if (e.which === 13 && newTaskInp.val() !== "") {
+        // console.log(`first i = ${i}`); // i is currently 0
+        // i++; // increasing this makes (i) one. this causes unexpected result in the local storage becuase your index will start at one instead of zero
+        // You can do this at a later time, maybe when you successfully add the todo list to other lists
+        // console.log(`second i = ${i}`);
+        let addedTask =
+            "<div class='tasks-list__i'><div class='round'> <input id='checkbox " +
+            `${i}` +
+            "' type='checkbox' name='checkbox' /><label id='label' for='checkbox " +
+            `${i}` +
+            "'><div class='task-list__txt'>" +
+            newTaskInp.val() +
+            "</div></label></div></div>";
+        myHtml += addedTask;
+        tasksList.html(myHtml);
+        try {
+            localStorage.setItem("myHtmlStored" + `${i}`, myHtml);
+            localStorage.setItem("lastIndex", i); // I use this so that we can keep track of our last todo list index
+            // console.log(
+            //   "local storage: ",
+            //   localStorage.getItem("myHtmlStored" + `${i}`)
+            // );
+            i++; // it is okay to increase our i here, our last index still remain intact
+            // console.log(`fixed new i = ${i}`);
+        } catch (e) {
+            console.log("What a nuisance");
+        }
+        newTaskInp.val("");
+        console.log(`first items left ${i}`);
+        iLeft.html(i + " items left");
     }
-    newTaskInp.val("");
-    iLeft.html(i + " items left");
-  }
-  $("input").on("click", function (e) {
-    if ($(this).is(":checked") && i !== 0) {
-      i--;
-      iLeft.html(i + " items left");
-    } else {
-      i++;
-      iLeft.html(i + " items left");
-    }
-  });
+    // console.log(myHtml);
+
+    // $("input").on("click", function (e) { // you need to fix this, I realised this is working on the input text area, and not on the radio buttons
+    //   if ($(this).is(":checked") && i !== 0) {
+    //     i--;
+    //     iLeft.html(i + " items left");
+    //   } else {
+    //     i++;
+    //     iLeft.html(i + " items left");
+    //   }
+    // });
 });
 
 themeIcon.on("click", function () {
-  body.toggleClass("dark-theme");
-  if (body.hasClass("dark-theme")) {
-    themeIcon.attr("src", "./images/icon-sun.svg");
-    bg.attr("src", "./images/bg-desktop-dark.jpg");
-  } else {
-    themeIcon.attr("src", "./images/icon-moon.svg");
-    bg.attr("src", "./images/bg-desktop-light.jpg");
-  }
+    body.toggleClass("dark-theme");
+    if (body.hasClass("dark-theme")) {
+        themeIcon.attr("src", "./images/icon-sun.svg");
+        bg.attr("src", "./images/bg-desktop-dark.jpg");
+    } else {
+        themeIcon.attr("src", "./images/icon-moon.svg");
+        bg.attr("src", "./images/bg-desktop-light.jpg");
+    }
 });
